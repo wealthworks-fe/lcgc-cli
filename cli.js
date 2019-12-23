4f90bc09dda0f5b6623537d4fb157e89bcf07d98
zhongxia <huangxiaodong@licaigc.com>
2019-12-23 16:31:22 +0800

    fix lint

--- cli.js
+++ cli.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-process.env.UMI_PLUGINS = require.resolve('./plugin')
-process.env.DISABLE_WARN = true
+process.env.UMI_PLUGINS = require.resolve('./plugin');
+process.env.DISABLE_WARN = true;
 
 // 基于 umi 的命令进行封装
-const umiBinPath = require.resolve('umi/bin/umi')
+const umiBinPath = require.resolve('umi/bin/umi');
 
 require('child_process').fork(umiBinPath, process.argv.slice(2), {
-  stdio: 'inherit'
-})
+  stdio: 'inherit',
+});
--- plugin.js
+++ plugin.js
@@ -1,6 +1,7 @@
 /**
  * 在这里注册命令工具需要的插件
  */
+
 const DEFAULT_CONFIG = {
   hash: true, // 生成的文件名加上 hash
   disableCSSModules: true, // 不使用 CSS Module 否则需要改代码
@@ -12,49 +13,50 @@ const DEFAULT_CONFIG = {
       '> 1%',
       'ie >= 9',
       'iOS >= 8',
-      'Android >= 4'
-    ]
-  }
-}
+      'Android >= 4',
+    ],
+  },
+};
 
 module.exports = (api, options = {}) => {
-  let { log, paths } = api
+  const { log, paths } = api;
 
   // 修改 umi 默认配置
-  api.modifyDefaultConfig(memo => {
-    return {
-      ...DEFAULT_CONFIG,
-      ...memo
-    }
-  })
+  api.modifyDefaultConfig(memo => ({
+    ...DEFAULT_CONFIG,
+    ...memo,
+  }));
 
   // 生成器插件注册
   api.registerPlugin({
     id: 'lcgc:command-generate',
-    apply: require('./plugins/generate')
-  })
+    // eslint-disable-next-line
+    apply: require('./plugins/generate'),
+  });
 
   // 自动注册多页面插件
   log.info(
-    `auto register plugin => [umi-plugin-mpa-pug] and pagesPath default 'pages'`,
-  )
+    "auto register plugin => [umi-plugin-mpa-pug] and pagesPath default 'pages'",
+  );
   api.registerPlugin({
     id: 'lcgc:mpa', // 插件名称随便，名字唯一即可
+    // eslint-disable-next-line
     apply: require('umi-plugin-mpa-pug'),
     opts: {
       pagesPath: paths.pagesPath,
-      ...options.mpa
-    }
-  })
+      ...options.mpa,
+    },
+  });
 
   // 如果有 cdn 配置，则注册 cdn 插件
   if (options.cdn) {
-    log.info(`auto register plugin => [umi-plugin-cdn]`)
+    log.info('auto register plugin => [umi-plugin-cdn]');
     // 注册七牛的插件
     api.registerPlugin({
       id: 'lcgc:cdn',
+      // eslint-disable-next-line
       apply: require('umi-plugin-cdn'),
-      opts: options.cdn
-    })
+      opts: options.cdn,
+    });
   }
-}
+};
